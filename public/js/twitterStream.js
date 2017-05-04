b02811625ace22f14eb890f00805bb62ca9d0780
Robert Spier <robert.spier@gmail.com>
2017-05-04 11:33:13 +0200

    Added comments to clarify codebase

--- public/js/twitterStream.js
+++ public/js/twitterStream.js
@@ -1,22 +1,41 @@
+// Functional programming, since OOP is a bit of an overkill for 2 functions on the client side.
+
+// Utility function to generate a random range
 function getRandomRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Initiate the socket connection on the client side
 function initSockets() {
+    // Check if we import the socket.io library
     if (io !== undefined) {
+        // Connect with the library on '/'
         const socket = io.connect('/');
+        // Save a let for sanitation purposes on the client side as well
         let last = null;
 
+        // Apply an event listener to the button (do this now, because we can hook it to the socket connection)
         document.getElementById('submit-button').addEventListener('click', e => {
+            // On click, get the new input string and send it to the server
             socket.emit('update string', document.getElementById('input-field').value)
         })
 
-        document.getElementById('waiting').remove();
+        // Next up are all socket related events
 
+        // As soon as we receive data from the twitter stream, handle it
         socket.on('twitter-stream', data => {
-            console.log('received data')
+            // We can remove the 'waiting for data' notice now, if it still exists, should be cleaner though.
+            if(document.getElementById('waiting')) {
+                document.getElementById('waiting').remove();
+            }
+
+            // If the last data we previously defined isn't the new data
             if(last !== data.src) {
+                // Set it as last
                 last = data.src
+
+                // Create a new image with the data as source, and randomly place it on the map between 10%/90%
+                // Also add a click event to emit a save image event to the server
                 const img = new Image();
                 img.src = data.src;
                 img.style.left = `${getRandomRange(10, 90)}%`;
@@ -24,23 +43,37 @@ function initSockets() {
                 img.addEventListener('click', e => {
                     socket.emit('save image', {src: e.target.src});
                 });
-
                 document.body.appendChild(img);
+
+                // Check if the maximum amount of images in the body has been reached
                 const images = document.querySelectorAll('body > img');
+
+                // If so, remove the oldest entry
                 if(images.length > 100) {
                     images[0].remove();
                 }
             }
+
+            // Wait 50ms before requesting a new image
+            // This isn't mandatory, but should be done for security purposes in case 100 users decide to visit the site at once
+            // It also makes debugging really easy
             setTimeout(() => {
                 socket.emit('request new image');
             }, 50)
         });
 
+        // Event to handle the updating the saved images list
         socket.on('update list', r => {
+            // Define the container to hold the images
             const container = document.getElementById('container-content');
+            // Empty it (sanitize)
             while(container.firstChild){
                 container.removeChild(container.firstChild);
             }
+
+            // Create a new image, and append it to the container
+            // This can probably be improved to only remove the oldest entry if needed, and only add the newest
+            // But hey, it works~
             for(let i = 0; i < r.length; i++) {
                 const img = new Image();
                 img.src= r[i].src;
@@ -48,12 +81,15 @@ function initSockets() {
             }
         });
 
+        // Handle erorr event
         socket.on('error', error => {
             console.log(`Error! message: ${error}`)
         });
 
+        // As soon as we're all initiated, we can ask the server for the first image, and start the loop
         socket.emit('request new image')
     }
 }
 
+// Let's go
 initSockets();
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Define the packages and requirements for the application to work.
 const twitter = require('twitter'),
 	  express = require('express'),
   	  app = express(),
@@ -6,6 +7,7 @@ const twitter = require('twitter'),
 	  io = require('socket.io').listen(server),
 	  CONFIG = require('./config.json');
 
+// Configure a twitter client
 const twit = new twitter({
 	consumer_key: CONFIG.consumer_key,
 	consumer_secret: CONFIG.consumer_secret,
@@ -13,6 +15,7 @@ const twit = new twitter({
 	access_token_secret: CONFIG.access_token_secret
 });
 
+// Configure the local (but global) variables to use between socket events and the twitter stream
 const local = {
 	memory: [],
 	output: null,
@@ -20,6 +23,7 @@ const local = {
 	update: false
 }
 
+// Validation function to see if the inputString has been changed
 function checkInput() {
 	if(local.update) {
 		local.update = false;
@@ -28,90 +32,129 @@ function checkInput() {
 	return false
 }
 
+// Initiate the server and call the socket connection
 function initServer() {
+	// Listen to the proccess.env.PORT or the configurated port
 	server.listen(process.env.PORT || CONFIG.port);
 	console.log(`Server is listening on port ${CONFIG.port}, or the local Heroku port`)
+	// Publish the public folder as a static folder to the client
 	app.use(express.static(__dirname + '/public'));
+	// Call the socket connection
 	initSockets()
 }
 
-function startStream() {
+// Initiate the socket connection
+function initSockets() {
+
+	// As soon as socket-io finds a new client, connect with it.
+	io.sockets.on('connection', socket => {
+
+		// Check if there are saved images in the sidebar, and send them to the new client if there are.
+		if(local.memory.length) {
+			socket.emit('update list', local.memory);
+		}
+
+		// Next up are all socket related events
+
+		// When 'update string' is received. Set the new inputString and change the update value to 'true'
+		socket.on('update string', string => {
+			local.inputString = string;
+			local.update = true;
+		});
+
+		// The client is ready to receive a new image, so we send the latest one saved from the stream, if not null
+		socket.on('request new image', () => {
+			// Check to see if there is any data at all, otherwise void
+			if(local.output !== null) {
+				socket.emit('twitter-stream', {'src': local.output});
+			}
+		})
+
+		// User wishes to save an image (click)
+		socket.on('save image', src => {
+			// Push the image src url to our local memory
+			local.memory.push(src)
+			// Check if the maximum number of images has been surpassed
+			if(local.memory.length > 15) {
+				// Remove the first entry from the array if so
+				local.memory.shift();
+			}
+			// Emit to update the list on the local client, broadcast to update for every other client
+			socket.emit('update list', local.memory);
+			socket.broadcast.emit('update list', local.memory);
+		});
+
+		// Handle error event
+        socket.on('error', error => {
+            console.log('error' + error)
+        });
+
+        // Handle disconnect event
+        socket.on('disconnect', () => {
+            console.log('A user disconnected')
+        });
+
+        // As soon as we've set up all events, send the 'connected' event to the client to complete the handshake
+		socket.emit('connected');
+
+	});
+}
+
+// Initiate the Twitter stream
+function initStream() {
+	// Hook into the stream API with a custom query and a low filter level (can be none or medium as well)
 	twit.stream('statuses/filter', {'track': local.inputString, 'filter_level':'low'}, stream => {
+		// Save the last entry for sanitation purposes
 		let last = null;
+
+		// As soon as there is new data, handle it
 		stream.on('data', data => {
+			// Validate if the user object is accesible (locked chars give an error)
 			if(data.user){
+				// Validate if the user profile image exists
 				if (data.user.profile_image_url !== null) {
+					// Apply the check if the image is the same one as last entry (duplicate streaming results)
 					if(data.user.profile_image_url !== last) {
+						// Update the last known image URL
 						last = data.user.profile_image_url;
+						// Update our output for the socket connection to use
 						local.output = data.user.profile_image_url;
 					}
 				}
 			}
 
+			// Handle error event
 			stream.on('error', error => {
 				return console.log(error);
 			});
 
+			// Handle limit event
 			stream.on('limit', limitMessage => {
 				return console.log(limitMessage);
 			});
 
+			// Handle warning event
 			stream.on('warning', warning => {
 				return console.log(warning);
 			});
 
+			// Handle disconnect event
 			stream.on('disconnect', disconnectMessage => {
 				return console.log(disconnectMessage);
 			});
 
+			// Check to see if the inputString has changed, every time there is new data
 			if(checkInput()) {
+				// If so, destroy the stream
 				stream.destroy();
-				startStream();
+				// And re-init the stream wit the new query
+				initStream();
 				console.log(`Restarted stream with query: ${local.inputString}`)
 			}
 		});
 	});
 }
 
-function initSockets() {
-	io.sockets.on('connection', socket => {
-		console.log('A new user connected!')
-
-		socket.on('update string', string => {
-			local.inputString = string;
-			local.update = true;
-		});
-
-		socket.on('request new image', () => {
-			if(local.output !== null) {
-				socket.emit('twitter-stream', {'src': local.output});
-			}
-		})
-
-		socket.on('save image', src => {
-			local.memory.push(src)
-			if(local.memory.length > 15) {
-				local.memory.shift();
-			}
-			socket.emit('update list', local.memory);
-			socket.broadcast.emit('update list', local.memory);
-		});
-
-		socket.emit('connected');
-
-		if(local.memory.length) {
-			socket.emit('update list', local.memory);
-		}
-			
-        socket.on('error', error => {
-            console.log('error' + error)
-        });
-
-        socket.on('disconnect', () => {
-            console.log('A user disconnected')
-        });
-	});
-}
-
+// Start the socket connection and Twitter stream
 initServer();
-startStream();
+initStream();
